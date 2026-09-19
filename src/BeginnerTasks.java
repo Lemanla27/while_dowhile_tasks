ae8e71fe96115712db1642ea7beb7407d3e9955c
Orkhan Mammadli <orkhan.mammadli@aztelekom.local>
2026-09-19 18:14:08 +0400

    Beginner tasks ended

---
+++ src/BeginnerTasks.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+public class BeginnerTasks {
+    static void main() {
+
+        //TASK-1
+        // İstifadəçidən daxil edilən ədədin faktorialını (n!) while dövrü ilə hesabla.
+        //5!=5*4*3*2*1
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Hansi ededin faktoriali tapilsin?");
+//        int num = sc.nextInt();//6
+//
+//        int multi = 1;
+//        while (num > 0) {
+//            multi = num * multi;//
+//            num--;
+//        }
+//        System.out.println(multi);
+
+
+        //TASK-2
+        //Verilmiş bir ədədin rəqəmlərinin sayını tap (məs: 4582 → 4 rəqəm).
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Eded daxil edin:");
+//        int num = sc.nextInt();
+//        int count = 0;
+//
+//        while (num > 0) {
+//            num = num /10;
+//            count++;
+//        }
+//        System.out.println(count);
+
+
+
+        //TASK-3
+        //Verilmiş bir ədədin rəqəmlərinin cəmini tap (məs: 123 → 1+2+3=6).
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Eded daxil edin:");
+//        int num = sc.nextInt();
+//
+//        int sum = 0;
+//
+//        while (num > 0) {//468
+//            int remainder = num % 10;
+//            sum = sum + remainder;
+//            num = num / 10;
+//        }
+//        System.out.println(sum);
+
+
+        //TASK-4
+//Verilmiş ədədin tərsini (əksini) tap (məs: 123 → 321).
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Eded daxil edin:");
+//        int num = scanner.nextInt();
+//
+//        String reverse = "";
+//
+//        while (num > 0) {//456
+//            int remainder = num % 10;//6//5
+//            reverse = reverse + remainder;//6+5
+//            num = num / 10;//45
+//
+//        }
+//        System.out.println(reverse);
+
+
+        //TASK-5
+        //do-while istifadə edərək istifadəçidən müsbət ədəd daxil etməsini
+        // tələb et — mənfi ədəd daxil edərsə, yenidən soruş.
+
+//        Scanner sc = new Scanner(System.in);
+//        int num;
+//        do {
+//            System.out.println("Musbet eded daxil et:");
+//            num = sc.nextInt();
+//
+//        } while (num < 0);
+//        System.out.println("Daxil edilen eded musbetdir");
+
+
+        //TASK-6
+        //do-while ilə sadə mətn menyusu yarat: istifadəçi "0" daxil edənə qədər proqram
+        //davam etsin.
+
+//        Scanner sc = new Scanner(System.in);
+//        // System.out.println("Sechim daxil edin:1 - Salamlama, 2 -Sagollashma, 0 - Chixish");
+//        int choice;
+//
+//        do {
+//            System.out.println("Sechim daxil edin:1 - Salamlama, 2 -Sagollashma, 0 - Chixish");
+//            choice = sc.nextInt();
+//            if (choice == 1) {
+//                System.out.println("Salam");
+//            } else if (choice == 2)
+//                System.out.println("Sagolun");
+//        } while (choice != 0);
+//        System.out.println("Chixish olundu");
+
+
+
+        //TASK-7
+        //İstifadəçidən daxil edilən bir ədədin vurma cədvəlini (1-dən 10-a qədər)
+        // while ile çap et.
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Eded daxil edin:");
+//
+//        int num1 = sc.nextInt();
+//        int num2 = 1;
+//
+//        while (num2 <= 10) {
+//            int mult = num1 * num2;
+//            System.out.println(num1 + "*" + num2 + "=" + mult);
+//            num2++;
+//        }
+//    }
+    }}
